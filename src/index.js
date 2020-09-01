029bf88b1057d9b9e180e6c3b1fbc375dc146e23
André Cruz <andremiguelcruz@msn.com>
2020-09-01 10:55:21 +0100

    fix: update p2p-websocket-star ip

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const createIpfs = (ipfs) => {
                     // ws-star0.ams = "ws-star0.ams.dwebops.pub ws-star.discovery.libp2p.io"
                     // ws-star1.par = "ws-star1.par.dwebops.pub"
                     // ws-star2.sjc = "ws-star2.sjc.dwebops.pub"
-                    Swarm: ['/dns4/ws-star1.par.dwebops.pub/tcp/443/wss/p2p-websocket-star'],
+                    Swarm: ['/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'],
                 },
             },
         });
